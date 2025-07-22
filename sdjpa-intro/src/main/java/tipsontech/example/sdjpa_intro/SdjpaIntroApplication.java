189b2730579a6a448a86187abfc26300c760e854
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-07-22 19:08:30 +0530

    created a new project

---
+++ sdjpa-intro/src/main/java/tipsontech/example/sdjpa_intro/SdjpaIntroApplication.java
@@ -0,0 +1,13 @@
+package tipsontech.example.sdjpa_intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdjpaIntroApplication.class, args);
+	}
+
+}
