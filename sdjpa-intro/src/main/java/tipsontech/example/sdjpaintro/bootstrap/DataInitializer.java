16ffd72113956d0e5900cd38d10c7356d623e261
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-07-24 23:33:33 +0530

    added bootstrap data

---
+++ sdjpa-intro/src/main/java/tipsontech/example/sdjpaintro/bootstrap/DataInitializer.java
@@ -0,0 +1,28 @@
+package tipsontech.example.sdjpaintro.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import tipsontech.example.sdjpaintro.domain.Book;
+import tipsontech.example.sdjpaintro.repositories.BookRepository;
+
+public class DataInitializer implements CommandLineRunner {
+
+    private final BookRepository bookRepository;
+
+    public DataInitializer(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Book bookSBIA = new Book("Spring Boot in Action", "978-1484249693", "Manning");
+        Book savedSBIA = bookRepository.save(bookSBIA);
+
+        Book bookSIA = new Book("Spring in Action", "978-1484250213", "Manning");
+        Book savedSIA = bookRepository.save(bookSIA);
+
+        bookRepository.findAll().forEach(book -> {
+            System.out.println("book Id : " + book.getId());
+            System.out.println("book title : " + book.getTitle());
+        });
+    }
+}
