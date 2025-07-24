61a27143b5213e72b774ae7f1adde4a783db5452
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-07-25 01:13:56 +0530

    added spring boot test

--- sdjpa-intro/src/test/java/tipsontech/example/sdjpaintro/SdjpaIntroApplicationTests.java
+++ sdjpa-intro/src/test/java/tipsontech/example/sdjpaintro/SdjpaIntroApplicationTests.java
@@ -1,11 +1,24 @@
 package tipsontech.example.sdjpaintro;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import tipsontech.example.sdjpaintro.repositories.BookRepository;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class SdjpaIntroApplicationTests {
 
+	@Autowired
+	BookRepository bookRepository;
+
+	@Test
+	void testBookRepository() {
+		long count = bookRepository.count();
+
+		assertThat(count).isGreaterThan(0);
+	}
 	@Test
 	void contextLoads() {
 	}
