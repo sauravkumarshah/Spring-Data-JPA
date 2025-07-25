1b4cd0744164167c03ecd6d68bd49a9751935fad
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-07-25 23:36:58 +0530

    initializing data with spring using liquibase

--- sdjpa-intro/src/main/java/tipsontech/example/sdjpaintro/domain/Book.java
+++ sdjpa-intro/src/main/java/tipsontech/example/sdjpaintro/domain/Book.java
@@ -14,7 +14,7 @@ import java.util.Objects;
 public class Book {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
