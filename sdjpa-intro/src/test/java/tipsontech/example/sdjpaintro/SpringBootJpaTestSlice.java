f5545556902a9c1b83574227b7b0694660abbfe2
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-07-25 21:57:14 +0530

    h2 mysql compatibility

--- sdjpa-intro/src/test/java/tipsontech/example/sdjpaintro/SpringBootJpaTestSlice.java
+++ sdjpa-intro/src/test/java/tipsontech/example/sdjpaintro/SpringBootJpaTestSlice.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.annotation.Commit;
@@ -17,6 +18,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
 @ComponentScan(basePackages = "tipsontech.example.sdjpaintro.bootstrap")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class SpringBootJpaTestSlice {
     @Autowired
     BookRepository bookRepository;
