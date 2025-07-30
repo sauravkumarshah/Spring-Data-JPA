d136e264fcf08ce12cd90b233a0f5d0667ee9194
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-07-31 00:55:34 +0530

    updated paging and sorting with hibernate

---
+++ 8-paging-and-sorting-using-hibernate/src/main/java/tipsontech/example/sdjpajdbctemplate/SdjpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package tipsontech.example.sdjpajdbctemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdjpaHibernateApplication.class, args);
+	}
+
+}
