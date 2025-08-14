4a5df0b07c8f5d50be228a0a72de4415ca83a73e
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-08-14 17:36:37 +0530

    lazy initiallize error

---
+++ 12-database-transactions-and-locking/src/main/java/tipsontech/example/orderservice/bootstrap/Bootstrap.java
@@ -0,0 +1,31 @@
+package tipsontech.example.orderservice.bootstrap;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import tipsontech.example.orderservice.domain.OrderHeader;
+import tipsontech.example.orderservice.repositories.OrderHeaderRepository;
+
+@Component
+public class Bootstrap implements CommandLineRunner {
+
+    @Autowired
+    private OrderHeaderRepository orderHeaderRepository;
+
+    @Transactional // without this the hibernate will throw an exception lazy initialization error
+    @Override
+    public void run(String... args) throws Exception {
+
+        OrderHeader orderHeader = orderHeaderRepository.findById(1L).get();
+
+        orderHeader.getOrderLines().forEach(orderLine -> {
+            System.out.println(orderLine.getProduct().getDescription());
+
+            orderLine.getProduct().getCategories().forEach(cat -> {
+                System.out.println(cat.getDescription());
+            });
+        });
+
+    }
+}
