f16b1e2120127a0bc6c18e4f417bba0814403534
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-08-14 22:22:16 +0530

    added pessimistic lock

--- 12-database-transactions-and-locking/src/main/java/tipsontech/example/orderservice/repositories/ProductRepository.java
+++ 12-database-transactions-and-locking/src/main/java/tipsontech/example/orderservice/repositories/ProductRepository.java
@@ -1,6 +1,8 @@
 package tipsontech.example.orderservice.repositories;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import tipsontech.example.orderservice.domain.Product;
 
 import java.util.Optional;
@@ -8,4 +10,8 @@ import java.util.Optional;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Optional<Product> findByDescription(String description);
+
+    @Override
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Product> findById(Long id);
 }
--- 12-database-transactions-and-locking/src/test/java/tipsontech/example/orderservice/repositories/ProductRepositoryTest.java
+++ 12-database-transactions-and-locking/src/test/java/tipsontech/example/orderservice/repositories/ProductRepositoryTest.java
@@ -4,19 +4,24 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
 import tipsontech.example.orderservice.domain.Product;
 import tipsontech.example.orderservice.domain.ProductStatus;
+import tipsontech.example.orderservice.services.ProductService;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 @ActiveProfiles("local")
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ComponentScan(basePackageClasses = {ProductService.class})
 class ProductRepositoryTest {
 
     @Autowired
     ProductRepository productRepository;
 
+    @Autowired
+    ProductService productService;
 
     @Test
     public void testGetCategory(){
@@ -47,11 +52,14 @@ class ProductRepositoryTest {
         product.setDescription("My Product");
         product.setProductStatus(ProductStatus.NEW);
 
-        Product savedProduct = productRepository.saveAndFlush(product);
+//        Product savedProduct = productRepository.saveAndFlush(product);
+        Product savedProduct = productService.saveProduct(product);
 
-        savedProduct.setQuantityOnHand(25);
+//        savedProduct.setQuantityOnHand(25);
 
-        Product savedProduct2 = productRepository.saveAndFlush(savedProduct);
+//        Product savedProduct2 = productRepository.saveAndFlush(savedProduct);
+
+        Product savedProduct2 = productService.updateQOH(savedProduct.getId(), 25);
 
         System.out.println(savedProduct2.getQuantityOnHand());
     }
