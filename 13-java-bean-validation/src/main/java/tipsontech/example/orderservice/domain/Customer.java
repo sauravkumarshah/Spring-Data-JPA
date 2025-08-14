29e93a52d34e10992d8b7f8c431effdc5cc9b0d8
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-08-14 23:42:24 +0530

    updated validation in address and customer

--- 13-java-bean-validation/src/main/java/tipsontech/example/orderservice/domain/Customer.java
+++ 13-java-bean-validation/src/main/java/tipsontech/example/orderservice/domain/Customer.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Version;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -11,10 +15,18 @@ import java.util.Set;
 @Entity
 public class Customer extends BaseEntity {
 
+    @Size(max = 50)
     private String customerName;
+
+    @Valid
     @Embedded
     private Address address;
+
+    @Size(max = 20)
     private String phone;
+
+    @Size(max = 255)
+    @Email
     private String email;
     @Version
     private Integer version;
