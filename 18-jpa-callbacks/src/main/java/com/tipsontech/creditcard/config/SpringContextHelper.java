ac583c6bc89c0376440a6bfb842c6e50c32f2156
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-08-18 22:34:26 +0530

    added spring context helper so that CreditCardJPACallback will be managed by spring context as earlier it was not

--- 18-jpa-callbacks/src/main/java/com/tipsontech/creditcard/config/SpringContextHelper.java
+++ 18-jpa-callbacks/src/main/java/com/tipsontech/creditcard/config/SpringContextHelper.java
@@ -1,2 +1,20 @@
-package com.tipsontech.creditcard.config;public class SpringContextHelper {
+package com.tipsontech.creditcard.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringContextHelper implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
 }
