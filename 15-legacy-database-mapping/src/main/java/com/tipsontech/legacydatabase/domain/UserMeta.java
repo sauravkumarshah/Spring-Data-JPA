206e4de92a0e228954572bbfba7c643aacb76157
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-08-18 11:54:56 +0530

    using lob annotation in a column

---
+++ 15-legacy-database-mapping/src/main/java/com/tipsontech/legacydatabase/domain/UserMeta.java
@@ -0,0 +1,53 @@
+package com.tipsontech.legacydatabase.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "wp_usermeta")
+public class UserMeta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "umeta_id")
+    private Long id;
+    @Column(name = "user_id")
+    private Long userId;
+    @Column(name = "meta_key")
+    private String metaKey;
+
+    @Column(name = "meta_value")
+    @Lob
+    private String metaValue;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getMetaKey() {
+        return metaKey;
+    }
+
+    public void setMetaKey(String metaKey) {
+        this.metaKey = metaKey;
+    }
+
+    public String getMetaValue() {
+        return metaValue;
+    }
+
+    public void setMetaValue(String metaValue) {
+        this.metaValue = metaValue;
+    }
+}
