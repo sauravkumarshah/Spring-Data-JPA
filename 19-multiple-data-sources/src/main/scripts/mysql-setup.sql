af1d1611611b87c719eeffa9bd8916c71d0a25f2
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-08-18 23:37:37 +0530

    added multiple data source properties

--- 19-multiple-data-sources/src/main/scripts/mysql-setup.sql
+++ 19-multiple-data-sources/src/main/scripts/mysql-setup.sql
@@ -1,38 +1,36 @@
--- card holder database
-
+-- cardholder database
 DROP DATABASE IF EXISTS cardholder;
 DROP USER IF EXISTS `cardholderadmin`@`%`;
 DROP USER IF EXISTS `cardholderuser`@`%`;
 CREATE DATABASE IF NOT EXISTS cardholder CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER IF NOT EXISTS `cardholderadmin`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
-    CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `cardholder`.* TO `cardholderadmin`@`%`;
+CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `cardholder`.* TO `cardholderadmin`@`%`;
 CREATE USER IF NOT EXISTS `cardholderuser`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
 GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON `cardholder`.* TO `cardholderuser`@`%`;
 FLUSH PRIVILEGES;
 
--- card database
 
+-- card database
 DROP DATABASE IF EXISTS card;
 DROP USER IF EXISTS `cardadmin`@`%`;
 DROP USER IF EXISTS `carduser`@`%`;
 CREATE DATABASE IF NOT EXISTS card CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER IF NOT EXISTS `cardadmin`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
-    CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `card`.* TO `cardadmin`@`%`;
+CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `card`.* TO `cardadmin`@`%`;
 CREATE USER IF NOT EXISTS `carduser`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
 GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON `card`.* TO `carduser`@`%`;
 FLUSH PRIVILEGES;
 
--- pan database
-
+-- card PAN database
 DROP DATABASE IF EXISTS pan;
 DROP USER IF EXISTS `panadmin`@`%`;
 DROP USER IF EXISTS `panuser`@`%`;
 CREATE DATABASE IF NOT EXISTS pan CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER IF NOT EXISTS `panadmin`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
-    CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `card`.* TO `panadmin`@`%`;
+CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `pan`.* TO `panadmin`@`%`;
 CREATE USER IF NOT EXISTS `panuser`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON `card`.* TO `panuser`@`%`;
+GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON `pan`.* TO `panuser`@`%`;
 FLUSH PRIVILEGES;
