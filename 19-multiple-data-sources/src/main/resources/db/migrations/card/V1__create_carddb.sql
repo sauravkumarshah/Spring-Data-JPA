eaab9e7e541495988bc9b3649fc79d7d71723818
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-08-19 00:33:39 +0530

    updated sql script

--- 19-multiple-data-sources/src/main/resources/db/migrations/card/V1__create_carddb.sql
+++ 19-multiple-data-sources/src/main/resources/db/migrations/card/V1__create_carddb.sql
@@ -5,12 +5,4 @@ CREATE TABLE credit_card (
     cvv varchar(20),
     expiration_date varchar(20),
     PRIMARY KEY (id)
-);DROP TABLE IF EXISTS credit_card_holder;
-
-  CREATE TABLE credit_card_holder (
-      id bigint not null auto_increment,
-      first_name varchar(30),
-      last_name varchar(30),
-      zip_code varchar(10),
-      PRIMARY KEY (id)
-  );
+);
