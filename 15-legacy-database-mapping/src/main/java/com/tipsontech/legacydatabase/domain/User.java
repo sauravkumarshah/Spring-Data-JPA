306a1c3e21621f22971468f00c42ed9676d02ef3
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-08-18 12:26:12 +0530

    creating database indexes

--- 15-legacy-database-mapping/src/main/java/com/tipsontech/legacydatabase/domain/User.java
+++ 15-legacy-database-mapping/src/main/java/com/tipsontech/legacydatabase/domain/User.java
@@ -9,7 +9,11 @@ import org.hibernate.validator.constraints.URL;
 import java.sql.Timestamp;
 
 @Entity
-@Table(name = "wp_users")
+@Table(name = "wp_users", indexes = {
+        @Index(name ="user_login_key", columnList = "user_login"),
+        @Index(name = "user_nicename", columnList = "user_nicename"),
+        @Index(name = "user_email", columnList = "user_email")
+})
 public class User {
 
     @Id
