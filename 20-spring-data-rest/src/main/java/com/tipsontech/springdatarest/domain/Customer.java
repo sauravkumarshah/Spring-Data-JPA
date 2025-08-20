3d2936f68f47d9abea643de2d43d249f7095f50d
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-08-20 18:03:15 +0530

    created a new branch for spring boot data rest

---
+++ 20-spring-data-rest/src/main/java/com/tipsontech/springdatarest/domain/Customer.java
@@ -0,0 +1,17 @@
+package com.tipsontech.springdatarest.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+    private UUID id;
+    private String name;
+}
