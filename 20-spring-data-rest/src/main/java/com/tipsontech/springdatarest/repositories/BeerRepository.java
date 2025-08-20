944f50d4b508c0df33036c6ff9b422b6e4b673c9
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-08-20 18:30:05 +0530

    customize url path

--- 20-spring-data-rest/src/main/java/com/tipsontech/springdatarest/repositories/BeerRepository.java
+++ 20-spring-data-rest/src/main/java/com/tipsontech/springdatarest/repositories/BeerRepository.java
@@ -5,10 +5,12 @@ import com.tipsontech.springdatarest.domain.BeerStyleEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.UUID;
 
 
+@RepositoryRestResource(path = "beer", collectionResourceRel = "beer")
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
     Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
 
