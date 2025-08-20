42c421c92329d71aac14cb21177a86a388ef5ae7
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-08-20 17:32:00 +0530

    save new credit card

--- 19-multiple-data-sources/src/main/java/com/tipsontech/creditcard/services/CreditCardService.java
+++ 19-multiple-data-sources/src/main/java/com/tipsontech/creditcard/services/CreditCardService.java
@@ -5,4 +5,6 @@ import com.tipsontech.creditcard.domain.creditcard.CreditCard;
 public interface CreditCardService {
 
     CreditCard getCreditCardById(Long id);
+
+    CreditCard saveCreditCard(CreditCard creditCard);
 }
--- 19-multiple-data-sources/src/main/java/com/tipsontech/creditcard/services/CreditCardServiceImpl.java
+++ 19-multiple-data-sources/src/main/java/com/tipsontech/creditcard/services/CreditCardServiceImpl.java
@@ -1,13 +1,48 @@
 package com.tipsontech.creditcard.services;
 
 import com.tipsontech.creditcard.domain.creditcard.CreditCard;
+import com.tipsontech.creditcard.domain.creditcardholder.CreditCardHolder;
+import com.tipsontech.creditcard.domain.creditcardpan.CreditCardPAN;
+import com.tipsontech.creditcard.repositories.cardholder.CreditCardHolderRepository;
+import com.tipsontech.creditcard.repositories.creditcard.CreditCardRepository;
+import com.tipsontech.creditcard.repositories.pan.CreditCardPANRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CreditCardServiceImpl implements CreditCardService {
+
+    private final CreditCardPANRepository creditCardPANRepository;
+    private final CreditCardHolderRepository creditCardHolderRepository;
+    private final CreditCardRepository creditCardRepository;
+
     @Override
     public CreditCard getCreditCardById(Long id) {
 
         return null;
+    }
+
+    @Override
+    public CreditCard saveCreditCard(CreditCard creditCard) {
+        CreditCard savedCC = creditCardRepository.save(creditCard);
+        savedCC.setFirstName(creditCard.getFirstName());
+        savedCC.setLastName(creditCard.getLastName());
+        savedCC.setZipCode(creditCard.getZipCode());
+        savedCC.setCreditCardNumber(creditCard.getCreditCardNumber());
+
+        creditCardHolderRepository.save(CreditCardHolder.builder()
+                .creditCardId(savedCC.getId())
+                .firstName(creditCard.getFirstName())
+                .lastName(creditCard.getLastName())
+                .zipCode(creditCard.getZipCode())
+                .build());
+
+        creditCardPANRepository.save(CreditCardPAN.builder()
+                .creditCardId(savedCC.getId())
+                .creditCardNumber(creditCard.getCreditCardNumber())
+                .build());
+
+        return savedCC;
     }
 }
